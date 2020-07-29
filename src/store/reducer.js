2c2bd0af01d6b0ef2899501ac6c3b1c3235a5dd9
MartaUlasen <marta.ulasen@gmail.com>
2020-07-29 09:11:26 +0300

    Updates

--- src/store/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import characterList from './characterList';
-import character from './character';
-
-export default combineReducers({
-    characterList,
-    character,
-});
